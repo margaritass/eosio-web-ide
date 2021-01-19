c871c4ae28795362705eb8dc8448fcb9196605b4
margaritass <mpandourova@yahoo.com>
2021-01-19 12:18:34 +0000

    Commit TicTacToe

---
+++ contract/tictactoe.cpp
@@ -0,0 +1,15 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+
+  class tictactoe : eosio::contract {
+    public:
+    // Use contract's constructor
+       using contract::contract;
+  
+       [[eosio::action]] void welcome( name host, name challenger){
+         require_auth( get_self() ); 
+         print("Welcome " host.name ", "  challenger.name);
+  };
+};
