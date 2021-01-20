33bc01adabeb734cbc31c71f4ce819c010cb3782
margaritass <mpandourova@yahoo.com>
2021-01-20 10:08:56 +0000

    New Actions

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -7,9 +7,42 @@ using namespace eosio;
     public:
     // Use contract's constructor
        using contract::contract;
-  
-       [[eosio::action]] void welcome( name host, name challenger){
+        tictactoe( name self, name first_receiver,datastream<const char>*ds): contract( self, first_receiver,ds);
+        game(get_self(), get_self().value);
+
+        [[eosio::action]] void welcome( name host, name challenger){
          require_auth(get_self()); 
-         print( "Welcome ",  host , ", ", challenger );
-  };
+         print( "Welcome ",  host , ", ", challenger ); };
+
+         [[eosio::action]] const create( name host, name challenger){
+         require_auth(get_self());
+         eosio::check(host != challenger, "Host And Challenger Should Be Different ");
+         auto itr = game.find(host.value + challenger.value)
+         eosio::check(itr == game.end(), "Game Between ", host, " and ", challenger "Already In Progress");
+         game.emplace(get_self(),[&](auto& new_row));
+           new_row.host = host;
+           new_row.challenger = challenger;
+          }; 
+         [[eosio::action]] const close( name host, name challenger){
+         require_auth(get_self()); 
+         eosio::check(host != challenger, "Host And Challenger Should Be Different ");
+         auto itr = game.find(host.value + challenger.value)
+         eosio::check(itr != game.end(), "Game Between ", host, " and ", challenger "Not Found");
+         itr = game.erase(itr);
+          };  
+
+    private:
+       struct [[eosio::table]] game_record{    
+       name host;
+       name challenger;
+       uint64_t primary_key const {return host.value + challenger.value ;};
+       EOSLIB_SERIALIZE(game_record, (host)(challenger))
+       };
+       typedef eosio::multi_index<
+         name("game"), //- Table Name
+         game_record  //- Table Structure
+         >game_index ;   //- Type Name
+  
+      
+ 
 };
